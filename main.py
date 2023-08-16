7a70316f487e773c5eb74657c37ed25c2ad35064
earayu <earayu@163.com>
2023-08-16 11:07:27 +0800

    add data2

--- main.py
+++ main.py
@@ -1,82 +1,82 @@
 import figure_util
 
-# figure_util.Draw(
-#     figureTitle="20% Read 80% Write",
-#     configs=[
-#         {
-#             'barName': 'MySQL',
-#             'lineName': 'MySQL Latency',
-#             'fileName': './data/mysql-20.csv',
-#         },
-#         {
-#             'barName': 'VTGate (0% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-20-0.csv',
-#         },
-#         {
-#             'barName': 'VTGate (66% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-20-66.csv',
-#         },
-#         {
-#             'barName': 'VTGate (100% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-20-100.csv',
-#         },
-#     ]
-# )
+figure_util.Draw(
+    figureTitle="20% Read 80% Write",
+    configs=[
+        {
+            'barName': 'MySQL',
+            'lineName': 'MySQL Latency',
+            'fileName': './data/mysql-20.csv',
+        },
+        {
+            'barName': 'VTGate (0% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
+            'fileName': './data/vtgate-20-0.csv',
+        },
+        {
+            'barName': 'VTGate (66% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
+            'fileName': './data/vtgate-20-66.csv',
+        },
+        {
+            'barName': 'VTGate (100% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
+            'fileName': './data/vtgate-20-100.csv',
+        },
+    ]
+)
 
-# figure_util.Draw(
-#     figureTitle="50% Read 50% Write",
-#     configs=[
-#         {
-#             'barName': 'MySQL',
-#             'lineName': 'MySQL Latency',
-#             'fileName': './data/mysql-50.csv',
-#         },
-#         {
-#             'barName': 'VTGate (0% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-50-0.csv',
-#         },
-#         {
-#             'barName': 'VTGate (66% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-50-66.csv',
-#         },
-#         {
-#             'barName': 'VTGate (100% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-50-100.csv',
-#         },
-#     ]
-# )
+figure_util.Draw(
+    figureTitle="50% Read 50% Write",
+    configs=[
+        {
+            'barName': 'MySQL',
+            'lineName': 'MySQL Latency',
+            'fileName': './data/mysql-50.csv',
+        },
+        {
+            'barName': 'VTGate (0% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
+            'fileName': './data/vtgate-50-0.csv',
+        },
+        {
+            'barName': 'VTGate (66% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
+            'fileName': './data/vtgate-50-66.csv',
+        },
+        {
+            'barName': 'VTGate (100% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
+            'fileName': './data/vtgate-50-100.csv',
+        },
+    ]
+)
 
-# figure_util.Draw(
-#     figureTitle="80% Read 20% Write",
-#     configs=[
-#         {
-#             'barName': 'MySQL',
-#             'lineName': 'MySQL Latency',
-#             'fileName': './data/mysql-80.csv',
-#         },
-#         {
-#             'barName': 'VTGate (0% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-80-0.csv',
-#         },
-#         {
-#             'barName': 'VTGate (66% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-80-66.csv',
-#         },
-#         {
-#             'barName': 'VTGate (100% Read Write Split Ratio)',
-#             'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
-#             'fileName': './data/vtgate-80-100.csv',
-#         },
-#     ]
-# )
+figure_util.Draw(
+    figureTitle="80% Read 20% Write",
+    configs=[
+        {
+            'barName': 'MySQL',
+            'lineName': 'MySQL Latency',
+            'fileName': './data/mysql-80.csv',
+        },
+        {
+            'barName': 'VTGate (0% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (0% Read Write Split Ratio)',
+            'fileName': './data/vtgate-80-0.csv',
+        },
+        {
+            'barName': 'VTGate (66% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (66% Read Write Split Ratio)',
+            'fileName': './data/vtgate-80-66.csv',
+        },
+        {
+            'barName': 'VTGate (100% Read Write Split Ratio)',
+            'lineName': 'VTGate Latency (100% Read Write Split Ratio)',
+            'fileName': './data/vtgate-80-100.csv',
+        },
+    ]
+)
 
 
 
